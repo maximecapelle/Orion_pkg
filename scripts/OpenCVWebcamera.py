c677cc095ccdb27b9b3ec42f131d44cd7ff93092
maximecapelle <max.luc.cap@gmail.com>
2023-07-11 13:09:25 -0700

    US sensor + USB camera on Orion

--- scripts/OpenCVWebcamera.py
+++ scripts/OpenCVWebcamera.py
@@ -4,14 +4,13 @@ import cv2
 
 
 # define a video capture object
-vid = cv2.VideoCapture(1)
+vid = cv2.VideoCapture(0)
 
 while(True):
 	
 	# Capture the video frame
 	# by frame
 	ret, frame = vid.read()
-	print(frame)
 
 	# Display the resulting frame
 	# cv2.imshow('frame', frame)
