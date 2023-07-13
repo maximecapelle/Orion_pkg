28da47613147f96d09ce86be64ad187723b58a0d
maximecapelle <max.luc.cap@gmail.com>
2023-07-13 12:13:55 +0200

    just added test image

--- scripts/Camera/OpenCVWebcamera.py
+++ scripts/Camera/OpenCVWebcamera.py
@@ -4,24 +4,30 @@ import cv2
 
 
 # define a video capture object
-vid = cv2.VideoCapture(0)
+# vid = cv2.VideoCapture(0)
 
-while(True):
-	
-	# Capture the video frame
-	# by frame
-	ret, frame = vid.read()
+img = cv2.imread('TestImage.jpg')
 
-	# Display the resulting frame
-	cv2.imshow('frame', frame)
-	
-	# the 'q' button is set as the
-	# quitting button you may use any
-	# desired button of your choice
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-
-# After the loop release the cap object
-vid.release()
-# Destroy all the windows
+cv2.imshow('TestImage', img)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# while(True):
+	
+# 	# Capture the video frame
+# 	# by frame
+# 	ret, frame = vid.read()
+
+# 	# Display the resulting frame
+# 	cv2.imshow('frame', frame)
+	
+# 	# the 'q' button is set as the
+# 	# quitting button you may use any
+# 	# desired button of your choice
+# 	if cv2.waitKey(1) & 0xFF == ord('q'):
+# 		break
+
+# # After the loop release the cap object
+# vid.release()
+# # Destroy all the windows
+# cv2.destroyAllWindows()
